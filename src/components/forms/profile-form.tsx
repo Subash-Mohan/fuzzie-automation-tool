dbefa9e372118784b9695d3c191fe7e617e901a0
Subash-Mohan <subashmohan75@gmail.com>
2024-07-20 15:16:23 +0530

    add name and email form in settings page

--- src/components/forms/profile-form.tsx
+++ src/components/forms/profile-form.tsx
@@ -6,6 +6,10 @@ import { zodResolver } from "@hookform/resolvers/zod"
 
 import { z } from "zod"
 import { EditUserProfileSchema } from "@/lib/types"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
+import { Loader2 } from "lucide-react"
 
 type Props = {}
 
@@ -20,7 +24,61 @@ const ProfileForm = (props: Props) => {
         },
     })
   return (
-    <div>ProfileForm</div>
+    <Form {...form}>
+        <form 
+            className="flex flex-col gap-6"
+            onSubmit={()=>{}}
+        >
+        <FormField
+          disabled={isLoading}
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">User full name</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  placeholder="Name"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Email</FormLabel>
+              <FormControl>
+                <Input
+                  {...field}
+                  disabled={true}
+                  placeholder="Email"
+                  type="email"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="self-start hover:bg-[#2F006B] hover:text-white "
+        >
+          {isLoading ? (
+            <>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Saving
+            </>
+          ) : (
+            'Save User Settings'
+          )}
+        </Button>
+        </form>
+    </Form>
   )
 }
 
