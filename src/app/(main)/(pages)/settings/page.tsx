1331c632d770c16bbcd2c81bd94e4d977ac847de
Subash-Mohan <subashmohan75@gmail.com>
2024-07-20 16:53:36 +0530

    add prisma and clerk

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,8 +1,41 @@
 import ProfileForm from "@/components/forms/profile-form"
+import ProfilePicture from "./_components/profile-picture"
+import { db } from "@/lib/db"
+import { currentUser } from '@clerk/nextjs/server'
 
 type Props = {}
 
-const Settings = (props: Props) => {
+const Settings = async (props: Props) => {
+    const authUser = await currentUser()
+  if (!authUser) return null
+    const removeProfileImage = async () => {
+        'use server'
+        const response = await db.user.update({
+          where: {
+            clerkId: authUser.id,
+          },
+          data: {
+            profileImage: '',
+          },
+        })
+        return response
+      }
+    
+      const uploadProfileImage = async (image: string) => {
+        'use server'
+        const id = authUser.id
+        const response = await db.user.update({
+          where: {
+            clerkId: id,
+          },
+          data: {
+            profileImage: image,
+          },
+        })
+    
+        return response
+      }
+    
   return (
     <div className="flex flex-col gap-4">
     <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
@@ -15,6 +48,11 @@ const Settings = (props: Props) => {
           Add or update your information
         </p>
       </div>
+      <ProfilePicture
+        onDelete={removeProfileImage}
+        onUpload={uploadProfileImage}
+        userImage={null}
+      />
       <ProfileForm />
     </div>
   </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import { ThemeProvider } from "@/providers/theme-provider";
 import "./globals.css";
-
+import { ClerkProvider } from '@clerk/nextjs';
+ 
 const font = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,17 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>       
-      </body>
-    </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={font.className}>
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+            </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>       
   );
 }
