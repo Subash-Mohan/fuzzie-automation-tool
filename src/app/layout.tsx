69d47e673c492042314a9c46e942d9f240a72428
Subash-Mohan <subashmohan75@gmail.com>
2024-08-09 16:18:18 +0530

    feat: Add drag and drop functionality for editor canvas cards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import { ThemeProvider } from "@/providers/theme-provider";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider } from "@clerk/nextjs";
 import ModalProvider from "@/providers/model-provider";
- 
+import { Toaster } from "@/components/ui/sonner";
 const font = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -24,17 +24,18 @@ export default function RootLayout({
       <html lang="en">
         <body className={font.className}>
           <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem
-              disableTransitionOnChange
-            >
-              <ModalProvider>
-                {children}
-              </ModalProvider>
-            </ThemeProvider>
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ModalProvider>
+              {children}
+              <Toaster />
+            </ModalProvider>
+          </ThemeProvider>
         </body>
       </html>
-    </ClerkProvider>       
+    </ClerkProvider>
   );
 }
